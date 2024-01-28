e716bdcecd61d49c8ffe40447eb600c6009e8507
ipavlopoulos <annis@aueb.gr>
2024-01-28 10:16:18 +0200

    Refactoring for pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='ndfu',
+    name='src',
     version='0.1.0',
     description='normalized distance from unimodality',
     author='John Pavlopoulos',
     author_email='annis@aueb.gr',
     url='https://github.com/ipavlopoulos/ndfu',
-    packages=['ndfu'],
+    packages=['src'],
 )
