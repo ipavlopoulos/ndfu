1b9e0ee91790e41c07065695c1626b1690c8309f
Ioannis Pavlopoulos <annis@aueb.gr>
2024-01-28 02:25:26 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ndfu',
+    version='0.1.0',
+    description='normalized distance from unimodality',
+    author='John Pavlopoulos',
+    author_email='annis@aueb.gr',
+    url='https://github.com/ipavlopoulos/ndfu',
+    packages=['lib'],
+)
